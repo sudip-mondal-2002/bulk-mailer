c618c370b70b90a9d2b52dd3bbb1ef4cf34c6281
Sudip Mondal <sudipmondal.2002@rediffmail.com>
2023-01-11 22:48:25 +0530

    add create template

--- frontend/pages/dashboard/template/index.tsx
+++ frontend/pages/dashboard/template/index.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import {UseTemplatesHook} from "../../../hooks/useTemplatesHook";
 
-export const createTemplate = ()=>{
-    return <div></div>
+export default function CreateTemplate (){
+    const {createTemplate} = UseTemplatesHook({})
+    const [code, setCode] = React.useState("")
+    const [name, setName] = React.useState("")
+    return <div>
+        <input value={name} onChange={(e)=>setName(e.target.value)} />
+        <textarea value={code} onChange={(e)=>setCode(e.target.value)}/>
+        <div dangerouslySetInnerHTML={{__html: code}} ></div>
+        <button onClick={()=>createTemplate(name, code)}>Upload</button>
+    </div>
 }
