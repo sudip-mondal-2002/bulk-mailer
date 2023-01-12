e97bfd16dd648a6ca0953a61468ae45c362c6773
Sudip Mondal <sudipmondal.2002@rediffmail.com>
2023-01-12 14:53:01 +0530

    add cors policy

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,11 +5,15 @@ import {errorHandler} from "./middlewares";
 import {NotFoundError} from "./errors";
 
 export const app = express()
-app.use(cors())
+app.use(cors({
+    allowedHeaders: [HttpHeaders.CONTENT_TYPE, HttpHeaders.AUTHORIZATION],
+    exposedHeaders: [HttpHeaders.AUTHORIZATION]
+}))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 // Import all routes
 import {authRouter, templateRouter, mailerRouter} from './routes'
+import {HttpHeaders} from "./enums";
 
 // Use all routes
 app.use('/auth', authRouter)
